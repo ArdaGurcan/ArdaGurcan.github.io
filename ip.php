5e78d044bcd828d81b5dabeb36093dcc474685da
ArdaGurcan <ardagurcan2004@gmail.com>
2021-07-13 16:29:29 +0300

    updated ip grabber

--- ip.php
+++ ip.php
@@ -1,7 +1,7 @@
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
 <?php 
 $url = $_SERVER['REQUEST_URI'];    
-if  ($url == "/main.php")
+if  ($url == "/ip.php")
 {
     echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">';
 }
