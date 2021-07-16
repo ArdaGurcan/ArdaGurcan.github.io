f5deda352d6bbf52d5756674662624e0387eaf53
ArdaGurcan <ardagurcan2004@gmail.com>
2021-07-16 16:25:19 +0300

    moved wall inside application func

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import os
 
 def application(environ,start_response):
     status = '200 OK'
-    html = 'html>\n' \
+    html = '<html>\n' \
            '<body>\n' \
            '<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">\n' \
            'Welcome to mod_wsgi Test Page\n' \
            '</div>\n' \
            '</body>\n' \
            '</html>\n'
+    os.system("cowsay -f tux | wall -n")
     response_header = [('Content-type','text/html')]
     start_response(status,response_header)
     return [html]
-
-os.system("cowsay -f tux | wall -n")
