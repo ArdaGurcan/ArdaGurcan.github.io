dc1eab98e7fec8923a295a3db690e566e6a1e2a6
ArdaGurcan <ardagurcan2004@gmail.com>
2021-07-24 11:52:06 +0300

    added name gen

---
+++ namegen.js
@@ -0,0 +1,178 @@
+let populationSize = 100000;
+
+class Population {
+    constructor(_target) {
+        this.fittest;
+        this.recordFitness=0;
+        this.words = [];
+        this.size = populationSize;
+        this.matingpool;
+        this.target = _target
+        for (let i = 0; i < this.size; i++) {
+            this.words[i] = new Word(null,_target);
+        }
+    }
+
+    evaluate() {
+        this.matingpool = [];
+        let maxFitness = 0;
+        for (let i = 0; i < this.size; i++) {
+            this.words[i].calcFitness();
+            let n = this.words[i].fitness;
+            if (n > maxFitness) {
+                maxFitness = n;
+                if (maxFitness > this.recordFitness) {
+                    this.recordFitness = maxFitness;
+                    this.fittest = this.words[i];
+                }
+            }
+        }
+        for (let i = 0; i < this.size; i++) {
+            for (
+                let j = 0;
+                j < (this.words[i].fitness / maxFitness) * 100;
+                j++
+            ) {
+                this.matingpool.push(this.words[i]);
+            }
+        }
+
+        // console.log(this.matingpool.length);
+    }
+
+    selection() {
+        let newWords = [];
+        if (this.fittest) {
+            newWords.push(new Word(this.fittest.dna,this.target));
+        }
+        for (let i = 0; i < this.size; i++) {
+            let parent1 = random(this.matingpool);
+            let parent2 = random(this.matingpool);
+            let child = parent1.dna.crossover(parent2.dna);
+            newWords.push(new Word(child,this.target));
+        }
+        this.words = newWords;
+    }
+}
+// let target;
+function strDistance(a,b)
+{
+    let distance = 0;
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < b.length; j++) {
+            if(a[i] != b[i])
+            {
+                distance++;
+            }
+        }
+    }
+
+    return distance
+}
+class Word {
+    constructor(dna,target) {
+        if (dna) {
+            this.dna =dna;
+        } else {
+            this.dna = new DNA(null,target);
+        }
+        if(target)
+        {
+            this.dna.target = target;
+        }
+        this.fitness;
+    }
+
+    calcFitness() {
+        this.fitness = Math.pow(1/(strDistance(this.dna.genes.join(""), this.dna.target)+1),5)
+        totalFitness+=this.fitness
+    }
+}
+let mutationRate = 0.01;
+
+class DNA {
+    constructor(genes, target) {
+        if(target)
+        {
+            this.target = target;
+        }
+        if (genes) {
+            this.genes = genes;
+        } else {
+            
+            this.genes = [];
+            for (let i = 0; i < this.target.length; i++) {
+                this.genes[i] = random("ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVWXYZabcçdegğhijklmnoöprqsştuüvyxz".split(""));
+            }
+        }
+    }
+
+    crossover(partner) {
+        let newGenes = [];
+        for (let i = 0; i < this.target.length; i++) {
+            if (random() > mutationRate) {
+                if (random() > 0.5) {
+                    newGenes.push(partner.genes[i]);
+                } else {
+                    newGenes.push(this.genes[i]);
+                }
+            } else {
+                newGenes.push(random("gprdaücnmesojtiul".split("")));
+            }
+        }
+
+        return new DNA(newGenes);
+    }
+}
+let population;
+let time = 0;
+let totalFitness = 0;
+let generation = 0;
+let allDone = false;
+let obstacles;
+let populations = [];
+function setup() {
+    populations.push(new Population("Arda"));
+    populations.push(new Population("Gürcan"));
+    populations.push(new Population("Games"));
+    populations.push(new Population("Projects"));
+    populations.push(new Population("Simulations"));
+}
+
+function draw() {
+    let complete = true;
+    for (let i = 0; i < populations.length; i++) {
+        if (
+            !populations[i].fittest ||
+            populations[i].fittest.dna.genes.join("") != populations[i].target
+        ) {
+            complete = false;
+            totalFitness = 0;
+
+            time++;
+
+            populations[i].evaluate();
+            populations[i].selection();
+            time = 0;
+            generation++
+        }
+    }
+    $(".name").text(populations[0].fittest.dna.genes.join("") + " " + populations[1].fittest.dna.genes.join(""))
+    $(".detail").text(populations[2].fittest.dna.genes.join("") + ", " + populations[3].fittest.dna.genes.join("") + ", " +populations[4].fittest.dna.genes.join("") + "...")
+    // console.log(
+    //     populations[0].fittest.dna.genes.join("") +
+    //         " " +
+    //         populations[1].fittest.dna.genes.join("") +
+    //         "\n" +
+    //         populations[2].fittest.dna.genes.join("") +
+    //         ", " +
+    //         populations[3].fittest.dna.genes.join("") +
+    //         ", " +
+    //         populations[4].fittest.dna.genes.join("") +
+    //         "..."
+    // );
+    if (complete) {
+        console.log(generation)
+        noLoop();
+    }
+}
